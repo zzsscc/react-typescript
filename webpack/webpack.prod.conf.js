edc3eaa0554536ac029b37eea3c6029e19eb2fdb
zhanshichao <zhanshichao@dianwoba.com>
2018-10-24 18:41:43 +0800

    feat(add): 生产环境采用多核打包，增大文件size限制

--- webpack/webpack.prod.conf.js
+++ webpack/webpack.prod.conf.js
@@ -1,13 +1,30 @@
 const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
 const merge = require('webpack-merge');
 const common = require('../webpack.common.conf');
 
 module.exports = merge(common, {
   devtool: 'source-map',     // 使用 source map，追踪错误和警告位置  // prod 使用source-map
   plugins: [
-    new UglifyJSPlugin({
-      sourceMap: true
-    }) // dev可以不需要
+    // new UglifyJSPlugin({
+    //   sourceMap: true
+    // }),
+    new ParallelUglifyPlugin({   // 多核打包, dev可以不需要
+      cacheDir: 'node_modules/.cache/uglifyjs_cache',
+      sourceMap: true,
+      uglifyJS: {
+        output: {
+          comments: false
+        },
+        compress: {
+          warnings: false
+        }
+      }
+    })
   ],
+  performance: {
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000
+  }
 });
